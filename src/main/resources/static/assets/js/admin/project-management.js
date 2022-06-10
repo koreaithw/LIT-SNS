ee8e348187f06ee99f23714f03d79e5e76e988fb
thirdK <dla0625@gmail.com>
2022-06-10 23:01:27 +0900

    Update project-management.html / css / js

---
+++ src/main/resources/static/assets/js/admin/project-management.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".side-bar").load("/src/main/resources/templates/admin/side-bar.html");
+});
+
+window.onload = function () {
+  $(".menu-box").eq(1).addClass("menu-box__select");
+};
+
+//기간 버튼
+$(".a-btn").on("click", function (e) {
+  e.preventDefault();
+  $(".period-button-wrap > .a-btn").removeClass("a-btn__selected");
+  $(this).addClass("a-btn__selected");
+});
+
+//date picker
+$(function () {
+  $(".datepicker").datepicker();
+});
+
+$(".calendar-icon-wrap").on("click", function () {
+  $input = $(this).prev("div").find("input");
+  console.log($input);
+  $input.trigger("focus");
+});
