22201a8f9cc53f6e7bf0c00030fe267647cde4be
thirdK <dla0625@gmail.com>
2022-06-10 23:02:43 +0900

    Change path login, side-bar, user-management

--- src/main/resources/static/assets/js/user-management.js
+++
@@ -1,9 +0,0 @@
-// 사이드바를 넣어줌
-// html작업이라 어쩔 수 없이 jquery로 넣었는데 나중에 수정해야 할 수 있음
-$(document).ready(function () {
-  $(".side-bar").load("/src/main/resources/templates/admin/side-bar.html");
-});
-
-window.onload = function () {
-  $(".menu-box").eq(0).addClass("menu-box__select");
-};
