b80275fa34b73f3d31c3adaf4c2d5b094570a469
YongWookLee <lsd1008@naver.com>
2022-06-13 16:07:27 +0900

    main, header, footer  front commit

---
+++ src/main/resources/static/assets/js/header.js
@@ -0,0 +1,33 @@
+//프로필 아이콘 클릭시 드롭다운 활성화
+$("._icon_profile").on("click",function(){
+
+if( $("._dropdown_wrapper").css("display") == 'none'){
+    $("._dropdown_wrapper").show();
+}else{
+    $("._dropdown_wrapper").hide();
+}
+})
+
+// 검색 버튼 클릭시 X버튼 노출
+$("._search_wrapper").on("click",function(){
+    $("._search_cancle").show()
+    $("._search_input").focus()
+    $("._search_span").hide()
+    $("._search_icon").hide()
+    
+    })
+
+// X 버튼 클릭시 원상복귀
+$("._search_cancle").on("click",function(e){
+    e.stopPropagation(); // 부모 요소 클릭 이벤트 금지
+    $("._search_cancle").hide()
+    $("._search_input").blur()
+    $("._search_span").show()
+    $("._search_icon").show()
+    
+    })
+
+
+
+
+
---
+++ src/main/resources/static/assets/js/main/main.js
@@ -0,0 +1,81 @@
+// let imgs = 2;
+// let now = 0;
+// let idx = 2;
+
+// function slide() {
+//     now = now == imgs ? 0 : now += 1; 
+
+//     switch(now){
+//         case 1:
+//             idx= 2;
+//             break;
+//         case 2:
+//             idx= 0;
+//             break;
+//         case 0:
+//             idx= 1;
+
+//     }
+    
+//     $("._ad_wrapper>img").eq(now - 1).css({"left": "0%", "z-index":"0"});
+//     $("._ad_wrapper>img").eq(now).css({"left": "-100%"});
+//     $("._ad_wrapper>img").eq(idx).css({"left": "0%", "z-index":"-10"});
+// }
+
+// function start() {
+//     setInterval(function () { slide()}, 2000);
+// }
+// start();
+
+// 뉴 방식
+let now = 0;
+let ani = 0;
+let len = $("._ad_wrapper>img").length;
+
+$("._ad_wrapper>img").css({"left": "-100%", "z-index":"0"}).eq(now).css({"left": "0%", "z-index":"1"});
+function slide() {
+    ani = (now+1) == len ? 0 : now + 1; 
+
+    $("._ad_wrapper>img").eq(now).css({"z-index":"0"});
+    $("._ad_wrapper>img").eq(ani).css({"left": "0%", "z-index":"1"});
+    setTimeout(function(){
+        $("._ad_wrapper>img").eq(now).css({"left": "-100%"});
+        now = ani;
+    }, 1000);
+}
+
+function start() {
+    setInterval(function () { slide()}, 3000);
+}
+start();
+
+
+
+    // ####################################################
+    // lit up(인증글 페이지 이동) 클릭 이벤트 Ajax 사용하세요
+
+    // $(".list1").on("click",function(e){
+    //     e.preventDefault();
+        
+    //     $.ajax({
+            
+
+
+    //     })
+    // })
+
+     // ####################################################
+    // lit(프로젝트 페이지 이동) 클릭 이벤트 Ajax 사용하세요
+
+    // $(".list2").on("click",function(e){
+    //     e.preventDefault();
+        
+    //     $.ajax({
+            
+
+            
+    //     })
+    // })
+
+    // ####################################################
+
