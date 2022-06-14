664d06bf00d1f606848fd5e3823f96ab46ceeb17
YongWookLee <lsd1008@naver.com>
2022-06-14 19:58:17 +0900

    Change_DB

--- src/main/java/com/example/lit/mapper/UserMapper.java
+++ src/main/java/com/example/lit/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.example.lit.mapper;
 
+import com.example.lit.domain.vo.UserVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-
+    //회원가입
+    public void insert(UserVO userVO);
 }
