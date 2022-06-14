b8d6854a35a3231e41001a7a94870321c91d1958
kjo7919 <pobo7917@gmail.com>
2022-06-15 08:59:04 +0900

    Merge branch 'master' of https://github.com/Code-Factory-Lit/Lit
    # Conflicts:
    #	src/main/resources/templates/changeinfo/changePw.html
    #	src/main/resources/templates/changeinfo/withdraw.html
    #	src/main/resources/templates/login/join.html
    #	src/main/resources/templates/login/login.html

---
+++ src/main/java/com/example/lit/controller/AdminController.java
@@ -0,0 +1,65 @@
+package com.example.lit.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/admin/*")
+@RequiredArgsConstructor
+public class AdminController {
+    //private final 의존성 주입으로 초기화할 객체
+
+    @GetMapping("/login")
+    public String login(){
+        log.info("***************************");
+        log.info("AdminController : login");
+        log.info("***************************");
+        return "/admin/login";
+    }
+
+    @GetMapping("/userManagement")
+    public String userManagement(){
+        log.info("***************************");
+        log.info("AdminController : userManagement");
+        log.info("***************************");
+        return "/admin/user-management";
+    }
+
+    @GetMapping("/approvedProjectManagement")
+    public String approvedProjectManagement(){
+        log.info("***************************");
+        log.info("AdminController : approvedProjectManagement");
+        log.info("***************************");
+        return "/admin/approved-project-management";
+    }
+
+    @GetMapping("/reportManagement")
+    public String  reportManagement(){
+        log.info("***************************");
+        log.info("AdminController : reportManagement");
+        log.info("***************************");
+        return "/admin/report-management";
+    }
+
+    @GetMapping("/waitingProjectManagement")
+    public String waitingProjectManagement(){
+        log.info("***************************");
+        log.info("AdminController : waitingProjectManagement");
+        log.info("***************************");
+        return "/admin/waiting-project-management";
+    }
+
+    @GetMapping("/reviewManagement")
+    public String reviewManagement(){
+        log.info("***************************");
+        log.info("AdminController : reviewManagement");
+        log.info("***************************");
+        return "/admin/review-management";
+    }
+
+
+}
