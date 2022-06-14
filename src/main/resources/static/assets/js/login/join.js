302cd076a2866414c01521e5b65e6f629e0e0e18
kjo7919 <pobo7917@gmail.com>
2022-06-14 16:19:29 +0900

    js 작업 완료

--- src/main/resources/static/assets/js/login/join.js
+++ src/main/resources/static/assets/js/login/join.js
@@ -1,90 +1,110 @@
-const $loginBtn = document.querySelector('#login_btn')
-const $idValue = document.querySelector('#idVal')
-const $passwordValue = document.querySelector('#pwVal')
-const $userName = document.querySelector('#userName')
-const $nickName = document.querySelector('#nickName')
+//8자리 이상, 대문자, 소문자, 숫자, 특수문자 모두 포함되어 있는 지 검사
+const loginBtn = $('#login_btn');
+const email = $('#email');
+const passwordValue = $('#pwVal');
+const userName = $('#userName')
+const nickName = $('#nickName');
+const onPwBtn = $('.onBtn');
 
-const loginBtn = function () {
-    if($idValue.value.indexOf('@') > 0 && $passwordValue.value.length >= 6 && $userName.value.length >= 3 && $nickName){
-        $loginBtn.className = 'buttonforjoin formEvent';
-    } else {
-        $loginBtn.className = 'buttonforjoin jonbuttoncolor';
-    }
+// 이메일 유효성 검사
+function checkEmail() {
+   var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+   if(filter.test(email.val())){
+      $('#emailCk').css("display", "grid");
+      $('#emailCk').attr('class', 'iconCheck');
+      return true;
+   } else if(email.val().length <= 0){
+      $('#emailCk').attr('class', 'iconCheck');
+      $('#emailCk').css("display", "none");
+      return false;
+   } else{
+      $('#emailCk').attr('class', 'iconX');
+      $('#emailCk').css("display", "grid");
+      return false;
+   }
 }
-$idValue.addEventListener('keyup', loginBtn);
-$passwordValue.addEventListener('keyup', loginBtn);
 
-
-var form = document.joinForm;
- 
-function join(){
-
-  if(!form.id.value){
-     alert("아이디를 입력해주세요");
-     form.id.focus();
-     return;
-  }
-  if(form.id.value.length < 4 || form.id.value.length > 16){
-     alert("아이디는 4자 이상, 16자 이하로 입력해주세요.");
-     return;
-  }
-  
-  if(!form.name.value){
-     alert("이름을 입력해주세요.");
-     form.name.focus();
-     return;
-  }
-  if(!form.password.value){
-     alert("비밀번호를 입력해주세요");
-     form.password.focus();
-     return;
-  }
-  
-  //8자리 이상, 대문자, 소문자, 숫자, 특수문자 모두 포함되어 있는 지 검사
-  let reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*?-]).{8,}$/;
-  let hangleCheck = /[ㄱ-ㅎ | ㅏ-ㅣ |가-힣]/
-  if(!reg.test(form.password.value)){
-     alert("비밀번호는 8자리 이상이어야 하며, 대문자/소문자/숫자/특수문자 모두 포함해야 합니다.");
-     form.password.focus();
-     return;
-  }
-  //같은 문자를 4번 사용할 수 없다.
-  if(/(\w)\1\1\1/.test(form.password.value)){
-     alert("같은 문자를 4번 이상 사용하실 수 없습니다.");
-     form.password.focus();
-     return;
-  }
-  
-  //비밀번호 안에 아이디가 있을 때 해당 인덱스를 가져오고 없으면 -1을 반환한다.
-  if(form.password.value.search(form.id.value) != -1){
-     alert("비밀번호에 아이디를 포함할 수 없습니다.");
-     form.password.focus();
-     return;
-  }
-  
-  //비밀번호에 한글이 있으면 안된다.
-  if(hangleCheck.test(form.password.value)){
-     alert("비밀번호에 한글을 사용할 수 없습니다.");
-     form.password.focus();
-     return;
-  }
-  
-  //비밀번호에 공백을 포함할 수 없다.
-  if(form.password.value.search(/\s/) != -1){
-     alert("비밀번호에 공백 없이 입력해주세요.");
-     form.password.focus()
-     return;
-  }
-  
-  if(form.password.value != form.password_re.value){
-     alert("패스워드를 확인해주세요.");
-     form.password.focus();
-     return;
-  }
-  
-  if(!form.id.readOnly){
-     alert("아이디 중복 검사를 진행해주세요.");
-     return;
-  }
-  form.submit();
+function checkUserName() {
+   if(userName.val().length <= 0){
+      $('#uNameCk').css("display", "none");
+      return false;
+   }else{
+      $('#uNameCk').css("display", "grid");
+      return true;
+   }
 }
+
+// 닉네임 중복 검사
+function checkNickName() {
+   if(nickName.val().length <= 0){
+      $('#nNameCk').css("display", "none");
+      return false;
+   }else if(nickName.val() == '비교할 값') { // 유저 닉네임 중복 확인
+      $('#nNameCk').attr('class', 'iconX');
+      $('#nNameCk').css("display", "grid");
+      return false;
+   } else{
+      $('#nNameCk').css("display", "grid");
+      return true;
+   }
+}
+
+// 비밀번호 보이기 버튼
+function onPwBtnFun() {
+   if (passwordValue.val().length > 0) {
+      onPwBtn.css('display', 'inline-block');
+      $('#pwCk').attr('class', 'iconX');
+      $('#pwCk').css("display", "grid");
+      if(onPwBtn.html() == "숨기기"){
+         onPwBtn.html("숨기기");
+      } else{
+         onPwBtn.html("비밀번호 표시");
+      }
+   } else {
+      onPwBtn.css('display', 'none');
+      $('#pwCk').css("display", "none");
+      return false;
+   }
+   checkPw()
+   return checkPw();
+}
+
+// 비밀번호 보이기
+onPwBtn.on('click', function () {
+   if (passwordValue.attr('type') == 'text') {
+      passwordValue.attr('type', 'password');
+      onPwBtn.html("비밀번호 표시");
+   } else {
+      passwordValue.attr('type', 'text');
+      onPwBtn.html("숨기기");
+   }
+});
+
+function checkPw() {
+   let hangleCheck = /[ㄱ-ㅎ | ㅏ-ㅣ |가-힣]/
+   if(!hangleCheck.test(passwordValue.val()) && passwordValue.val().length >= 6){
+      $('#pwCk').css("display", "grid");
+      $('#pwCk').attr('class', 'iconCheck');
+      return true;
+   }else{
+      return false;
+   }
+}
+
+// 버튼 이벤트 효과적용 및 색상 변경
+function loginBtnF() {
+   var ckE = checkEmail();
+   var ckU = checkUserName();
+   var ckN = checkNickName();
+   var ckP = onPwBtnFun();
+   if (ckE && ckU && ckN && ckP) {
+      loginBtn.attr("class", "buttonforjoin formEvent");
+   } else {
+      loginBtn.attr("class", "buttonforjoin jonbuttoncolor");
+   }
+}
+
+email.keyup(loginBtnF)
+userName.keyup(loginBtnF)
+nickName.keyup(loginBtnF)
+passwordValue.keyup(loginBtnF)
--- src/main/resources/static/assets/js/login/login.js
+++ src/main/resources/static/assets/js/login/login.js
@@ -1,12 +1,14 @@
-const $loginBtn = document.querySelector('#login_btn')
-const $idValue = document.querySelector('#email')
-const $passwordValue = document.querySelector('#pwVal')
-const loginBtn = function () {
-    if($idValue.value.indexOf('@') > 0 && $passwordValue.value.length >= 6){
-        $loginBtn.className = 'buttonforjoin formEvent';
+const loginBtn = $('#login_btn');
+const email = $('#email');
+const passwordValue = $('#pwVal');
+var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+const loginBtnF = function () {
+    if(passwordValue.val().length >= 6 && filter.test(email.val())){
+        loginBtn.attr("class", "buttonforjoin formEvent");
     } else {
-        $loginBtn.className = 'buttonforjoin jonbuttoncolor';
+        loginBtn.attr("class", "buttonforjoin jonbuttoncolor");
     }
 }
-$idValue.addEventListener('keyup', loginBtn)
-$passwordValue.addEventListener('keyup', loginBtn)
+email.keyup(loginBtnF)
+passwordValue.keyup(loginBtnF)
