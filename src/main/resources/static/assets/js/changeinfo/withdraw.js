38cb37594ec7107d2c1abe7b3763387820ab0acd
kjo7919 <pobo7917@gmail.com>
2022-06-15 11:54:41 +0900

    js 작업끝

--- src/main/resources/static/assets/js/changeinfo/withdraw.js
+++ src/main/resources/static/assets/js/changeinfo/withdraw.js
@@ -4,9 +4,14 @@ const pwBtn = $('#btnNoOff');
 
 pw.keyup(function(){
     
-    if(pw.val() == "1111"){
+    if(pw.val() == "1111"){ // 비번 유효성 검사
+        $ ('._checkArea1').html('');
         pwBtn.attr('class', 'buttonOn');
-    }else{
+    } else if(pw.val().length <= 0){
+        $ ('._checkArea1').html('');
+    } else{
+        $ ('._checkArea1').html('비밀번호가 일치하지 않습니다.');
+        $('._checkArea1').css('color', 'red');
         pwBtn.attr('class', 'buttonStyle');
     }
 
