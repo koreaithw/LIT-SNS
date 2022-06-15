0dcb5fb28b93334c2e275ded21e7ec5336f16d5a
kjo7919 <pobo7917@gmail.com>
2022-06-15 13:00:17 +0900

    회원탈퇴 alter 예/아니오 작업 완료

--- src/main/resources/static/assets/js/changeinfo/withdraw.js
+++ src/main/resources/static/assets/js/changeinfo/withdraw.js
@@ -17,6 +17,14 @@ pw.keyup(function(){
 
 })
 
+function removeCheck() {
+    if (confirm("정말 삭제하시겠습니까??") == true){    //확인
+        alert("회원탈퇴가 완료되었습니다.");
+    }else{   //취소
+        return false;
+    }
+   }
+
 pwBtn.on("click", function(){
-    alert("회원탈퇴가 완료되었습니다.");
+    removeCheck();
 });
