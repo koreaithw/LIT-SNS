38a5c37eb3a3261a5fd7d623c423569c9602195a
thirdK <dla0625@gmail.com>
2022-06-15 17:23:37 +0900

    Update DAO add method

--- src/main/java/com/example/lit/domain/dao/user/achievement/AchievementDAO.java
+++ src/main/java/com/example/lit/domain/dao/user/achievement/AchievementDAO.java
@@ -1,4 +1,23 @@
 package com.example.lit.domain.dao.user.achievement;
 
+import com.example.lit.domain.vo.user.FollowVO;
+import com.example.lit.domain.vo.user.achievement.AchievementVO;
+import com.example.lit.mapper.user.achievement.AchievementMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class AchievementDAO {
+    AchievementMapper achievementMapper;
+
+    //메달 추가
+    public void register(FollowVO followVO){ achievementMapper.insert(followVO); }
+    //메달 전체 해금 유무 (?)
+    public List<AchievementVO> getList(Long userNumber){ return achievementMapper.getList(userNumber); }
+    //메달 하나 보기
+    public AchievementVO read(Long userNumber){ return achievementMapper.select(userNumber); }
+
 }
