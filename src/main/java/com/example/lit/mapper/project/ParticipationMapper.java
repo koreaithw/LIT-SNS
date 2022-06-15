c40731e0b38f011bd88b61bf9b1cc84d0197375d
YongWookLee <lsd1008@naver.com>
2022-06-15 16:21:55 +0900

    Mapper Method

--- src/main/java/com/example/lit/mapper/project/ParticipationMapper.java
+++ src/main/java/com/example/lit/mapper/project/ParticipationMapper.java
@@ -1,4 +1,7 @@
 package com.example.lit.mapper.project;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface ParticipationMapper {
 }
--- src/test/java/com/example/lit/mapper/UserMapperTests.java
+++ src/test/java/com/example/lit/mapper/UserMapperTests.java
@@ -3,7 +3,6 @@ package com.example.lit.mapper;
 
 import com.example.lit.domain.vo.user.UserVO;
 import com.example.lit.mapper.user.UserMapper;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,21 +19,17 @@ public class UserMapperTests {
     public void insertTest(){
         UserVO userVO = new UserVO();
         userVO.setEmail("abc");
-        userVO.setName("asds");
-        userVO.setNickname("azxcsd");
+        userVO.setName("asd");
+        userVO.setNickname("azx");
         userVO.setPassword("1234");
         userMapper.insert(userVO);
     }
 
-    @Test
-    public void loginTest(){
-        UserVO userVO = new UserVO();
-        userVO.setEmail("abc");
-        userVO.setName("asds");
-        userVO.setNickname("azxcsd");
-        userVO.setPassword("1234");
-        userMapper.insert(userVO);
-    }
+//    @Test
+//    public void loginTest(){
+//        String email=""
+//        userMapper.insert(userVO);
+//    }
 
 
 }
