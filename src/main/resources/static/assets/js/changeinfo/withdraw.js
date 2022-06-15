77fc2ae299cf8deb7d241a8ec0c3c0fa53dc6ba1
kjo7919 <pobo7917@gmail.com>
2022-06-15 16:14:24 +0900

    no message

--- src/main/resources/static/assets/js/changeinfo/withdraw.js
+++ src/main/resources/static/assets/js/changeinfo/withdraw.js
@@ -1,15 +1,30 @@
 
+const pw = $('#_withdrawlPassword');
+const pwBtn = $('#btnNoOff');
 
-
-$("._withdrawlPassword").keyup(function(){
-    if($("._withdrawlPassword").val()){
-        $("._withdrawl_button").attr("disabled",false)
-    }
-
-    if(!$("._withdrawlPassword").val()){
-        $("._withdrawl_button").attr("disabled",true)
+pw.keyup(function(){
+    
+    if(pw.val() == "1111"){ // 비번 유효성 검사
+        $ ('._checkArea1').html('');
+        pwBtn.attr('class', 'buttonOn');
+    } else if(pw.val().length <= 0){
+        $ ('._checkArea1').html('');
+    } else{
+        $ ('._checkArea1').html('비밀번호가 일치하지 않습니다.');
+        $('._checkArea1').css('color', 'red');
+        pwBtn.attr('class', 'buttonStyle');
     }
 
 })
 
+function removeCheck() {
+    if (confirm("정말 삭제하시겠습니까??") == true){    //확인
+        alert("회원탈퇴가 완료되었습니다.");
+    }else{   //취소
+        return false;
+    }
+   }
 
+pwBtn.on("click", function(){
+    removeCheck();
+});
