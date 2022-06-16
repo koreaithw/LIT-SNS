249893079f7a653662317307afd49e9b3f568722
thirdK <dla0625@gmail.com>
2022-06-16 10:52:47 +0900

    Update controller

--- src/main/java/com/example/lit/controller/UserRestController.java
+++ src/main/java/com/example/lit/controller/UserRestController.java
@@ -3,6 +3,7 @@ package com.example.lit.controller;
 import com.example.lit.service.User.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
     private final UserService userService;
 
+    //=============== 프로필 수정 ====================
     //모달 프로필 사진 바꾸기 -> 반환타입 미정
     @PostMapping("/changeImg")
     public void changeImg(){
     }
     
+    //닉네임 중복확인
     @PostMapping("/checkNickName")
     public String checkNickName(){
         return null;
     }
 
-
+    @PatchMapping("/changeInfo")
+    public String changeInfo(){
+        return null;
+    }
 
 }
