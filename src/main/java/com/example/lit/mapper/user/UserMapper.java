01fe093e8a97780a227516f33228278859235309
thirdK <dla0625@gmail.com>
2022-06-17 23:03:54 +0900

    no message

--- src/main/java/com/example/lit/mapper/user/UserMapper.java
+++ src/main/java/com/example/lit/mapper/user/UserMapper.java
@@ -26,7 +26,7 @@ public interface UserMapper {
 
     // 관리자 검색
     public List<UserVO> userSearch(SearchDTO searchDTO);
-    
+
     //카카오 로그인/회원가입
     public void kakaoInsert(UserVO userVO);
     //이메일 중복체크
