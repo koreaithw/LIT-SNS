e9466d3d1b5a53725829d8e24997d7ad34023470
kjo7919 <pobo7917@gmail.com>
2022-06-22 22:24:42 +0900

    no message

--- src/main/java/com/example/lit/controller/MainController.java
+++ src/main/java/com/example/lit/controller/MainController.java
@@ -1,24 +1,17 @@
 package com.example.lit.controller;
 
-import com.example.lit.domain.vo.review.LikeVO;
-import com.example.lit.domain.vo.review.ReviewVO;
-import com.example.lit.domain.vo.user.FollowVO;
 import com.example.lit.service.review.LitUpService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class MainController {
     //의존성 주입 객체 선언하기
-    private final LitUpService litUpService;
 
     @GetMapping("/main")
     public String main(){
