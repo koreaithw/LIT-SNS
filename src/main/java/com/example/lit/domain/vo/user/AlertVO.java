cbae5e67518cd25bd631f6c8fd0fcc6a11ec3fe6
YongWookLee <lsd1008@naver.com>
2022-06-27 02:48:49 +0900

    asd

--- src/main/java/com/example/lit/domain/vo/user/AlertVO.java
+++ src/main/java/com/example/lit/domain/vo/user/AlertVO.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class AlertVO {
+    private Long alertNumber;
     private Long alertUser;
     private Long userNumber;
     private String typeAlert;
