28dc8c8493721217254dcf8df47c43a1e5f0b98e
Runarell <arckrich@gmail.com>
2022-06-27 11:03:58 +0900

    20220627 프로젝트 상세 오류 수정

--- src/main/java/com/example/lit/service/project/LitServiceImple.java
+++ src/main/java/com/example/lit/service/project/LitServiceImple.java
@@ -61,7 +61,7 @@ public class LitServiceImple implements LitService{
 
     @Override
     public ProjectDTO read(ProjectDTO projectDTO) {
-        Long projectNumber = projectDTO.getUserNumber();
+        Long projectNumber = projectDTO.getProjectNumber();
         Long userNumber = projectDTO.getUserNumber();
 
         ParticipationVO participationVO = new ParticipationVO();
@@ -71,8 +71,9 @@ public class LitServiceImple implements LitService{
        projectDTO = projectDAO.read(projectNumber);                                     // 기본 데이터 겟
             projectDTO.setReviewCount( projectDAO.reviewTotal(projectNumber) );         // 게시물 토탈
             projectDTO.setParticipationCount( projectDAO.challengeTotal(projectNumber));// 참가자 토탈
-            projectDTO.setParticipationStatus( participationDAO.select( participationVO ) );
+            projectDTO.setParticipationStatus( participationDAO.select( participationVO ).getStatus() );
             projectDTO.setUserNumber( userNumber );
+
         return projectDTO;
     }
 
