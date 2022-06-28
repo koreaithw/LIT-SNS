a985d0d43452eaa2becd12e8a82b0f219206ac44
thirdK <dla0625@gmail.com>
2022-06-28 21:06:13 +0900

    Update Main sort Update project apply_count / participation

--- src/test/java/com/example/lit/service/project/LitServiceTests.java
+++ src/test/java/com/example/lit/service/project/LitServiceTests.java
@@ -1,6 +1,7 @@
 package com.example.lit.service.project;
 
 import com.example.lit.domain.vo.ListDTO;
+import com.example.lit.domain.vo.project.ParticipationVO;
 import com.example.lit.domain.vo.project.ProjectDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -29,5 +30,18 @@ class LitServiceTests {
         ListDTO listDTO = new ListDTO();
         listDTO.setOrder("new");
 //        litService.getMainList(listDTO).stream().map(ProjectDTO::toString).forEach(log::info);
+    }
+
+    @Test
+    public void joinTest(){
+        log.info("*************************************");
+        log.info("LitServiceTests : joinTest");
+        log.info("*************************************");
+        ParticipationVO participationVO = new ParticipationVO();
+        participationVO.setStatus(1L);
+        participationVO.setProjectNumber(11L);
+        participationVO.setUserNumber(6L);
+        litService.join(participationVO);
+
     }
 }
